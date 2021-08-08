f0b28dbfe3fb0ecf79d7935152acba631125ef85
ryezzz <ryezzz@gmail.com>
2021-08-08 14:53:03 -0400

    fixing dark mode

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,11 +4,8 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation">
-  <Link to="/">Portfolio</Link>
-  <Link to="/contact">Contact</Link>
-
-{<ThemeChanger/>}
-
+    <Link to="/contact">Contact</Link>
+    <ThemeChanger/>
   </nav>
 
 )
