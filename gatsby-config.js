b15eaad2cad7e392a6c3f07c785974e933a42c8c
ryezzz <ryezzz@gmail.com>
2021-12-09 17:22:30 -0500

    removing ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,14 +34,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164743872-1",
-        head: true,
-      }
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Randomly Colorful Portfolio`,
